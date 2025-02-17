8bb961fc783cca83a699b8add2247eb5fb1ed442
7ching <jill921@gmail.com>
2025-02-17 21:53:48 +0800

    56

---
+++ Intervals/56.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        for i in intervals:
+            if not merged or merged[-1][1] < i[0]:
+                merged.append(i)
+            else:
+                merged[-1][1] = max(merged[-1][1], i[1])
+        return merged
+
+intervals = [[1,2],[3,6],[8,10],[15,18]]
+print(Solution().merge(intervals))  # Output: [[1,6],[8,10],[15,18]]
